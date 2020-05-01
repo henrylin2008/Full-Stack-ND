d5c9c6b1c2cc9279823e3d07be83c7e6bcb54943
hlin <henrylin2008@yahoo.com>
2020-05-01 08:36:06 -0700

    try..except...finally

--- Database/todoapp/app.py
+++ Database/todoapp/app.py
@@ -1,9 +1,14 @@
-from flask import Flask, render_template,request, redirect, url_for, jsonify 
+from flask import Flask, render_template,request, redirect, url_for, jsonify, abort
 from flask_sqlalchemy import SQLAlchemy 
+import sys 
+from flask_migrate import Migrate
 
 app = Flask(__name__)
 app.config['SQLALCHEMY_DATABASE_URI'] = 'postgresql://mango:mango@localhost:5433/todoapp' # database configuration
+app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False # disable overhead  
 db = SQLAlchemy(app)  # linking SQLAlchemy with the app
+
+migrate = Migrate(app, db) # initized migration, update/downgrade database
 
 class Todo(db.Model): 
 	__tablename__ = 'todos'  # table name
@@ -17,14 +22,28 @@ db.create_all() # sync table and model, ensure talbe is created from decleared m
 
 @app.route('/todos/create', methods=['POST'])
 def create_todo():
-	description = request.get_json()['description'] # get content from description field 
-	# description = request.form.get('description', '') # '': default empty string if nothing comes in
-	todo = Todo(description=description) # create a object 
-	db.session.add(todo) # add todo to pending stage
-	db.session.commit() # push it to the database 
-	return jsonify({ # return json object
-		'description': todo.description
-		})
+	error = False
+	body = {}
+	try: 
+		description = request.get_json()['description'] # get (json) content from description field 
+		# description = request.form.get('description', '') # '': default empty string if nothing comes in
+		todo = Todo(description=description) # create a object 
+		db.session.add(todo) # add todo to pending stage
+		db.session.commit() # push it to the database 
+		body['description'] = todo.description
+	except: 
+		error = True
+		db.session.rollback()
+		print(sys.exc_info())
+	finally:
+		db.session.close()
+	if error:
+		abort (400)
+	else: 
+		return jsonify(body)
+	# return jsonify({ # return json data to the client 
+	# 	'description': todo.description
+		# })
 	#redirect(url_for('index')) # return to index page when it's done 
 
 @app.route('/')
