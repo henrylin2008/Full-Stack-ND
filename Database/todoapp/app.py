bc092c2e0b5250995a22805766b6c7bfa40e5eb1
hlin <henrylin2008@yahoo.com>
2020-05-02 10:54:23 -0700

    updating Todo items

--- Database/todoapp/app.py
+++ Database/todoapp/app.py
@@ -4,21 +4,22 @@ import sys
 from flask_migrate import Migrate
 
 app = Flask(__name__)
-app.config['SQLALCHEMY_DATABASE_URI'] = 'postgresql://mango:mango@localhost:5433/todoapp' # database configuration
+app.config['SQLALCHEMY_DATABASE_URI'] = 'postgresql://mango:mango@localhost:5432/todoapp' # database configuration
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False # disable overhead  
 db = SQLAlchemy(app)  # linking SQLAlchemy with the app
 
-migrate = Migrate(app, db) # initized migration, update/downgrade database
+migrate = Migrate(app, db) # initized migration, update/downgrade database 
 
 class Todo(db.Model): 
 	__tablename__ = 'todos'  # table name
 	id = db.Column(db.Integer, primary_key=True) # id attribute
 	description = db.Column(db.String(), nullable=False) # description attribute 
+	completed = db.Column(db.Boolean, nullable=False, default=False)
 
 	def __repr__(self): # debugging statement
 		return f'<Todo {self.id} {self.description}>'
 
-db.create_all() # sync table and model, ensure talbe is created from decleared model 
+# db.create_all() # sync table and model, ensure talbe is created from decleared model 
 
 @app.route('/todos/create', methods=['POST'])
 def create_todo():
@@ -46,9 +47,23 @@ def create_todo():
 		# })
 	#redirect(url_for('index')) # return to index page when it's done 
 
+
+@app.route('/todos/<todo_id>/set-completed', methods=['POST'])
+def set_completed_todo(todo_id):
+	try: 
+		completed = request.get_json()['completed']
+		todo = Todo.query.get(todo_id) # grab todo_id 
+		todo.completed = completed
+		db.session.commit() # push to db
+	except:
+		db.session.rollback() # if any errors, rollback the changes 
+	finally:
+		db.session.close() # close out the session regards what happened 
+	return redirect(url_for('index')) # grab refreshed list items in index page 
+
 @app.route('/')
 def index():
-	return render_template('index.html', data = Todo.query.all())	
+	return render_template('index.html', data = Todo.query.order_by('id').all())	
 	# Todo.query.all(): select * from Todo (in DB) 
 
 if __name__ == '__main__':
