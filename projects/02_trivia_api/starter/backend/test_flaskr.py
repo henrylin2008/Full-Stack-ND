62bd3d87943844eec74743f6b62e78b64d6ec826
hlin <henrylin2008@yahoo.com>
2020-06-05 11:07:54 -0700

    test cases: test_get_categories, test_get_paginated_questions, test_404_sent_requesting_beyond_valid_page

--- projects/02_trivia_api/starter/backend/test_flaskr.py
+++ projects/02_trivia_api/starter/backend/test_flaskr.py
@@ -29,10 +29,36 @@ class TriviaTestCase(unittest.TestCase):
         """Executed after reach test"""
         pass
 
-    """
-    TODO
-    Write at least one test for each test for successful operation and for expected errors.
-    """
+    def test_get_categories(self):
+        """Test case for retrieve categories function"""
+        response = self.client().get('/categories')
+        data = json.loads(response.data)
+
+        self.assertEqual(response.status_code, 200)
+        self.assertEqual(data['success'], True)
+        self.assertTrue(data['categories'])
+
+    def test_get_paginated_questions(self):
+        """Test case for retrieve paginated questions"""
+        response = self.client().get('/questions')
+        data = json.loads(response.data)
+
+        self.assertEqual(response.status_code, 200)
+        self.assertEqual(data['success'], True)
+        self.assertTrue(len(data['questions']) <= 10)
+        self.assertTrue(data['categories'])
+        self.assertTrue(data['total_questions'])
+
+    def test_404_sent_requesting_beyond_valid_page(self):
+        response = self.client().get('/questions&page=1000')
+        data = json.loads(response.data)
+
+        self.assertEqual(response.status_code, 404)
+        self.assertEqual(data['success'], False)
+        self.assertEqual(data['error'], 404)
+        self.assertEqual(data['message'], 'Resource not found')
+
+    # def test_delete_question(self):
 
 
 # Make the tests conveniently executable
