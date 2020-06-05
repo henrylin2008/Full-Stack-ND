a43d731b219572eea03d169ca9f07f88c35c4f7c
hlin <henrylin2008@yahoo.com>
2020-06-05 12:44:06 -0700

    added following cases: test_delete_question, test_404_sent_deleting_non_existing_question, test_create_a_question,

--- projects/02_trivia_api/starter/backend/test_flaskr.py
+++ projects/02_trivia_api/starter/backend/test_flaskr.py
@@ -50,6 +50,7 @@ class TriviaTestCase(unittest.TestCase):
         self.assertTrue(data['total_questions'])
 
     def test_404_sent_requesting_beyond_valid_page(self):
+        """Test case for 404 sending request beyond total pages"""
         response = self.client().get('/questions&page=1000')
         data = json.loads(response.data)
 
@@ -58,7 +59,44 @@ class TriviaTestCase(unittest.TestCase):
         self.assertEqual(data['error'], 404)
         self.assertEqual(data['message'], 'Resource not found')
 
-    # def test_delete_question(self):
+    def test_delete_question(self):
+        """Test case to delete a question"""
+        total_questions_before_delete = len(Question.query.all())
+        response = self.client().delete('/questions/6')
+        data = json.loads(response.data)
+
+        self.assertEqual(response.status_code, 200)
+        self.assertEqual(data['success'], True)
+        self.assertEqual(data['deleted'], 6)
+        deleted_question = total_questions_before_delete - data['total_questions']
+        self.assertEqual(deleted_question, 1)
+
+    def test_404_sent_deleting_non_existing_question(self):
+        """Test case for deleting a non-existing question"""
+        response = self.client().delete('/question/1000')
+        data = json.loads(response.data)
+
+        self.assertEqual(response.status_code, 404)
+        self.assertEqual(data['success'], False)
+        self.assertEqual(data['error'], 404)
+        self.assertEqual(data['message'], 'Resource not found')
+
+    def test_create_a_question(self):
+        total_questions_before_add = len(Question.query.all())
+        new_question = {
+            "question": "adding a test question",
+            "answer": "answer",
+            "category": "3",
+            "difficulty": "1"
+        }
+        response = self.client().post('questions', json=new_question)
+        data = json.loads(response.data)
+
+        self.assertEqual(response.status_code, 200)
+        self.assertEqual(data['success'], True)
+        self.assertTrue(data['created'])
+        num_of_added_question = data['total_questions'] - total_questions_before_add
+        self.assertEqual(num_of_added_question, 1)
 
 
 # Make the tests conveniently executable
