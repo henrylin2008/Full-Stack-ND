c2c8cb15553c591df9631b3c9ba8392c6c303e25
hlin <henrylin2008@yahoo.com>
2020-06-23 09:59:22 -0700

    Validating auth with header

---
+++ Identity_n_Access_Management/validating_Auth_header.py
@@ -0,0 +1,35 @@
+from flask import Flask, request, abort
+from functools import wraps
+
+def get_token_auth_header(): 
+	if 'Authorization' not in request.headers:
+		abort(401)
+
+	auth_header = request.headers['Authorization']
+	header_parts = auth_header.split(' ')
+
+	if len(header_parts) != 2: # abort(401) if len != 2 
+		abort(401)
+	elif header_parts[0].lower() != 'bearer': # if first word in header_parts != 'bearer'
+		abort(401)
+
+	return header_parts[1] # return second item in header_parts 
+
+def requires_auth(f): 
+	@wraps(f)
+	def wrapper(*args, **kwargs): 
+		jwt = get_token_auth_header()
+		return f(jwt, *args, **kwargs)
+	return wrapper
+
+app = Flask(__name__)
+
+@app.route('/headers')
+@requires_auth
+def headers():
+	jwt = get_token_auth_header()
+	print(jwt)
+	return "not implemented"
+
+# export FLASK_APP=app.py
+# flask run --reload
